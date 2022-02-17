367eeccf877349d4cbf583cacdfaa19eb100ec3a
wafopoi <99897093+wafopoi@users.noreply.github.com>
2022-02-18 00:16:04 +0800

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> My Home Page </title>
+  </head>
+  <body>
+  <h1> Welcome to My Website! I am Mikhail Joshua! </h1>
+  <ul>
+    <li><a href="index.php" target="_blank"> HOME </a></li>
+    <li><a href="/LT2/Homepage" target= "_blank"> LEARNING TASK 2 </a> </li>
+  </ul>
+  </body>
+</html>
