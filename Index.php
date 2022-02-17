d3c8fa4c5f708c102dfc8da922f62494c5a3503b
wafopoi <99897093+wafopoi@users.noreply.github.com>
2022-02-18 00:22:37 +0800

    Update Index.php

--- Index.php
+++ Index.php
@@ -7,7 +7,7 @@
   <h1> Welcome to My Website! I am Mikhail Joshua! </h1>
   <ul>
     <li><a href="index.php" target="_blank"> HOME </a></li>
-    <li><a href="/LT2/Homepage" target= "_blank"> LEARNING TASK 2 </a> </li>
+    <li><a href="/LT2/Homepage.html" target= "_blank"> LEARNING TASK 2 </a> </li>
   </ul>
   </body>
 </html>
