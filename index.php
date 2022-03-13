7f907632de5c27c8c0c293aa63bf2585d4918feb
wafopoi <99897093+wafopoi@users.noreply.github.com>
2022-03-13 23:56:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <ul>
     <li><a href="index.php" target="_blank"> HOME </a></li>
     <li><a href="/LT2/Homepage.html" target= "_blank"> LEARNING TASK 2 </a> </li>
+    <li><a href="/LT3/index.html" target= "_blank"> LEARNING TASK 3 </a> </li>
   </ul>
   </body>
 </html>
