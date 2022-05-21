f47c98f69cb70b3dc7eceb7e390219c2049e06ff
wafopoi <99897093+wafopoi@users.noreply.github.com>
2022-05-21 15:01:47 +0800

    Update index.php

--- CLE1/index.php
+++ CLE1/index.php
@@ -230,7 +230,6 @@
                 <div class="error-message"></div>
                 <div class="sent-message">Your message has been sent. Thank you!</div>
               </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
             </form>
 
           </div>
